2dc7c8e69c1b2c75dc1898a7468cdf5eb93c9022
Nestor Castillo <nes_23@yahoo.com>
2015-12-27 17:20:13 -0700

    Initial Commit

---
+++ pokedex-by-ncc/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  pokedex-by-ncc
+//
+//  Created by Nestor Castillo on 12/27/15.
+//  Copyright © 2015 Nestor Castillo. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
