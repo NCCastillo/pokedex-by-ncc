ad7e5b7d2c3898987a1b4edd165a18760ad2b195
Nestor Castillo <nes_23@yahoo.com>
2016-01-01 21:14:05 -0700

    Part 13 - downloading and parsing data

---
+++ pokedex-by-ncc/Constants.swift
@@ -0,0 +1,14 @@
+//
+//  Constants.swift
+//  pokedex-by-ncc
+//
+//  Created by Nestor Castillo on 1/1/16.
+//  Copyright © 2016 Nestor Castillo. All rights reserved.
+//
+
+import Foundation
+
+let URL_BASE = "http://pokeapi.co"
+let URL_POKEMON = "/api/v1/pokemon/"
+
+typealias DownloadComplete = () -> ()
