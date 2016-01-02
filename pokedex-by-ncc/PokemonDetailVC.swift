ee63a6f735aa22a9d3c1136c7c40bc891e3504f8
Nestor Castillo <nes_23@yahoo.com>
2016-01-01 19:37:15 -0700

    IBOUTLETS detail view controller

--- pokedex-by-ncc/PokemonDetailVC.swift
+++ pokedex-by-ncc/PokemonDetailVC.swift
@@ -10,7 +10,18 @@ import UIKit
 
 class PokemonDetailVC: UIViewController {
     @IBOutlet weak var nameLbl: UILabel!
-    
+    @IBOutlet weak var mainImg: UIImageView!
+    @IBOutlet weak var descriptionLbl: UILabel!
+    @IBOutlet weak var typeLbl: UILabel!
+    @IBOutlet weak var defenseLbl: UILabel!
+    @IBOutlet weak var heightLbl: UILabel!
+    @IBOutlet weak var pokedexLbl: UILabel!
+    @IBOutlet weak var weightLbl: UILabel!
+    @IBOutlet weak var baseAttackLbl: UILabel!
+    @IBOutlet weak var currentEvoImg: UIImageView!
+    @IBOutlet weak var nextEvoImg: UIImageView!
+    @IBOutlet weak var evoLbl: UILabel!
+
     var pokemon: Pokemon!
     
     override func viewDidLoad() {
@@ -21,18 +32,10 @@ class PokemonDetailVC: UIViewController {
 
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
-        // Dispose of any resources that can be recreated.
     }
     
-
-    /*
-    // MARK: - Navigation
-
-    // In a storyboard-based application, you will often want to do a little preparation before navigation
-    override func prepareForSegue(segue: UIStoryboardSegue, sender: AnyObject?) {
-        // Get the new view controller using segue.destinationViewController.
-        // Pass the selected object to the new view controller.
+    @IBAction func backBtnPressed(sender: AnyObject) {
+        dismissViewControllerAnimated(true, completion: nil)
     }
-    */
 
 }
